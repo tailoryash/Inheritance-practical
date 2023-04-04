4444db99df0e4b2d74dda5be6e502efbc06f9c48
tailoryash <theyash.tailor@gmail.com>
2023-04-04 15:20:43 +0530

    Main.java updated

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(5055, "Yash Tailor", 22, 100000, "JAVA");
+
+        Employee employee2 = new Employee(5056, "Yash Fofdiya", 25, 1200000, "JAVA");
+
+        employee1.EmployeeDetails();
+        System.out.println("---------------------------------------");
+        employee2.EmployeeDetails();
+
+    }
+}
